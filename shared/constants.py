508f2c3f491714501293eb6e337821ec11ec1997
Tavleen Kaur <122363068+Tavleen1203@users.noreply.github.com>
2024-08-11 09:33:17 +0530

    Constants

---
+++ shared/constants.py
@@ -0,0 +1,8 @@
+
+BUFFER_SIZE = 4096  
+MULTICAST_GROUP = '224.1.1.1'  
+MULTICAST_PORT = 5007  # Multicast port
+#FILE_TRANSFER_PORT = 6000  # Port for direct file transfers
+FILE_TRANSFER_PORT = 6002  # Change to an unused port
+
+ADMIN_AUTH_TOKEN = "admin_secret_token"  
